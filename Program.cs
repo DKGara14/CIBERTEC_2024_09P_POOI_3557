cc902484977718a9c71dc9b1d885dff7dd9a74e0
DKGara14 <142203278+DKGara14@users.noreply.github.com>
2024-09-12 15:36:42 -0500

    Tarea01_Problema_1_1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filmacion doc = new Filmacion();
+            doc.codigo = 001;
+            doc.titulo = "Divertidamente";
+            doc.duracion = 180;
+            doc.precioSoles = 10;
+
+            Console.WriteLine("Codigo: " + doc.codigo);
+            Console.WriteLine("Titulo: " + doc.titulo);
+            Console.WriteLine("Duracion de la pelicula en Min: " + doc.duracion);
+            Console.WriteLine("Precio en Soles: " + doc.precioSoles);
+            Console.WriteLine("precio en Dolares: " + doc.precioDolares());
+
+            Console.ReadKey();
+        }
+    }
+}
