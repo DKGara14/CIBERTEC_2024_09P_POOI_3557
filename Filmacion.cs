f9994f554365ee5ce09f0a4d28dca9b3c45f400d
DKGara14 <142203278+DKGara14@users.noreply.github.com>
2024-09-12 15:25:59 -0500

    POOI_Tarea01_Problema_1_1
    Problema_1_1

---
+++ Filmacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea01
+{
+    internal class Filmacion
+    {
+        public int codigo { set; get; }
+        public string titulo { set; get; }
+        public int  duracion { set; get; }
+        public double precioSoles { set; get; }
+
+        public double  precioDolares()
+        {
+            return precioSoles * 3.59;
+        }
+    }
+}
